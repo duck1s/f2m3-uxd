09eed1462c42ff00caa6aaacc34eb1e151a84631
Duck1s <32653@ma-web.nl>
2022-03-16 09:55:33 +0100

    initial commit

---
+++ les4-charts/script.js
@@ -0,0 +1,23 @@
+var stars = [135850, 52122, 148825, 16939, 9763];
+var frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
+var ctx = document.getElementById("myChart");
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: frameworks,
+        datasets: [{
+            label: 'GitHub Stars',
+            data: stars,
+            backgroundColor: [ "rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)","rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)" ] ,
+            borderColor: [  "rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)","rgba(153, 102, 255, 1)", ], 
+            borderWidth: 1,
+            fill: true,
+            lineTension: 0
+        }]
+            
+    },
+    options: {
+
+    }
+})
